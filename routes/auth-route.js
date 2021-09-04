22c7a2f056d43f11f13010478e684d2adbe331bb
Ajea Smith <smithajea@rocketmail.com>
2021-09-04 09:55:00 -0400

    setup routes for auth route

---
+++ routes/auth-route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+const router = express.Router();
+
+router.get('/register', )
+
+module.exports = router;
