46031d1382ff55be191a001e24a4d0cc595e6dd2
Ajea Smith <smithajea@rocketmail.com>
2021-09-11 09:13:46 -0400

    setup router routes

--- src/comment/CommentRouter.js
+++ src/comment/CommentRouter.js
@@ -4,7 +4,11 @@ const router = express.Router();
 const UserService = require('./UserService');
 const passport = require('passport');
 
-router.post('/:storyId/comment', validate, async (req, res) => {});
+router.post('/:storyId/comment/add', validate, async (req, res) => {
 
+});
+router.delete('/:commentId', async (req, res) => {
+
+})
 
 module.exports = router;
