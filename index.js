246e506e007ea895ef378d991a3d9499a3bd655e
Ajea Smith <smithajea@rocketmail.com>
2021-09-19 22:21:48 -0400

    added cors options

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ require('dotenv').config();
 //Set up mongodb connection
 InitalDBServer().catch((err) => console.error(err));
 
-// passport
+var corsOptions = {
+	origin: 'http://localhost:3000',
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
