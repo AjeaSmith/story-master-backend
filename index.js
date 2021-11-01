6d0f6820233db81940ebd102d1799215fd453a31
Ajea Smith <smithajea@rocketmail.com>
2021-11-01 02:59:29 -0400

    added cors options

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ let corsOptions = {
 	credentials: true,
 };
 // ------------ Middlewares (General) --------------
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
